f62acefa3ff037597b20e51aaa8523ecbb5fe404
MARLON XAVIER DELGADO RUIZ <marlonxavi6@gmil.com>
2025-02-17 11:11:09 -0500

    ADD: se crea la clase Emergencia

---
+++ src/controller/Emergencia.java
@@ -0,0 +1,45 @@
+package controller;
+
+public class Emergencia {
+    private String ubicacion;
+    private int nivelGravedad;
+    private int tiempoReespuesta;
+    
+    public Emergencia(String ubicacion, int nivelGravedad, int tiempoReespuesta) {
+        this.ubicacion = ubicacion;
+        this.nivelGravedad = nivelGravedad;
+        this.tiempoReespuesta = tiempoReespuesta;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getNivelGravedad() {
+        return nivelGravedad;
+    }
+
+    public void setNivelGravedad(int nivelGravedad) {
+        this.nivelGravedad = nivelGravedad;
+    }
+
+    public int getTiempoReespuesta() {
+        return tiempoReespuesta;
+    }
+
+    public void setTiempoReespuesta(int tiempoReespuesta) {
+        this.tiempoReespuesta = tiempoReespuesta;
+    }
+
+    @Override
+    public String toString() {
+        return "Emergencia [ubicacion=" + ubicacion + ", nivelGravedad=" + nivelGravedad + ", tiempoReespuesta="
+                + tiempoReespuesta + "]";
+    }
+
+    
+}
