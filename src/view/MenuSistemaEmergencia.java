249db6820e03a051e1005076819ae091be022f6a
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-02-27 17:15:00 -0500

    fix:  se corrigen los nombres del imórt y del this,  al mismo tiempo se borra un metodo que no se usaba

--- src/view/MenuSistemaEmergencia.java
+++ src/view/MenuSistemaEmergencia.java
@@ -1,20 +1,19 @@
 package view;
 import java.util.Scanner;
-import controller.ControladorEmergencias;
+import controller.SistemaEmergencias;
 
 
  //Clase que representa el menú del sistema de emergencias.
  
 public class MenuSistemaEmergencia {
     private Scanner scanner;
-    private ControladorEmergencias controlador;
 
     
      // Constructor que inicializa el menú con el controlador.
      
     public MenuSistemaEmergencia() {
         this.scanner = new Scanner(System.in);
-        this.controlador = new ControladorEmergencias();
+        this.controller = new SistemaEmergencias();
     }
 
     
