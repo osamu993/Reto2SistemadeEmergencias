1fbf70f7ced87893a50119896d8ec89feac476d8
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-02-28 21:04:32 -0500

    fead: se crean nuevas interfaces

--- src/model/interfaces/IServicioEmergencia.java
+++ src/model/interfaces/IServicioEmergencia.java
@@ -11,6 +11,31 @@ public interface IServicioEmergencia {
     void gastarCombustible(int cantidad);
     void tanquearCombustible(int cantidad);
     //void atenderEmergencia(Emergencia emergencia); 
+   
+        /**
+         * Método para desplegar una unidad al lugar de la emergencia.
+         * @param ubicacion Ubicación de la emergencia.
+         */
+        void desplegarUnidad(String ubicacion);
+        
+        /**
+         * Método para evaluar la situación en la emergencia.
+         */
+        void evaluarSituacion();
+        
+        /**
+         * Método para obtener el estado del servicio de emergencia.
+         * @return Estado actual del servicio en formato de texto.
+         */
+        String getEstado();
+        
+        /**
+         * Método para liberar el recurso después de atender una emergencia.
+         */
+        void liberarRecurso();
 
+        
+    }
 
-}
+    
+
