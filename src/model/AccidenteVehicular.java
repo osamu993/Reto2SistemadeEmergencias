d59ff370ca4351cefd53b7f03e9e3fd69c89e031
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-03-18 09:43:25 -0500

    feat: Mensaje mejorado en atenderEmergencia() para incluir la ubicación del accidente. Mayor claridad en evaluarEstado() con un mensaje más detallado. Preparado para refactorizar si Emergencia implementa Responder, reduciendo código repetido.

--- src/model/AccidenteVehicular.java
+++ src/model/AccidenteVehicular.java
@@ -3,20 +3,20 @@ package model;
 import controller.Responder;
 import utils.NivelGravedad;
 
-public class AccidenteVehicular extends Emergencia implements Responder{
+public class AccidenteVehicular extends Emergencia implements Responder {
 
-    public AccidenteVehicular(String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta){
+    public AccidenteVehicular(String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta) {
         super("Accidente Vehicular", ubicacion, nivelGravedad, tiempoReespuesta);
     }
 
     @Override
     public void atenderEmergencia() {
-        System.out.println("Paramedicos en camino!!!");
+        System.out.println("🚑 Paramedicos en camino al accidente vehicular en " + getUbicacion() + "!!!");
     }
 
     @Override
     public void evaluarEstado() {
-        System.out.println("Revisar signos vitales de los involucrados");
+        System.out.println("📋 Evaluando signos vitales de los involucrados en el accidente.");
     }
-
 }
+
