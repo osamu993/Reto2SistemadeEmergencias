4d1217f374d71cae65148db5514881be9b76a80f
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-03-18 09:30:21 -0500

    feat: Nuevo método notificarEmergenciaResuelta(Emergencia emergencia) para informar que una emergencia ha sido resuelta. Nuevo método notificarRecursosReasignados para informar cuando los recursos han sido reasignados.

--- src/model/observer/SujetoEmergencias.java
+++ src/model/observer/SujetoEmergencias.java
@@ -3,7 +3,33 @@ package model.observer;
 import model.Emergencia;
 
 public interface SujetoEmergencias {
+
+    /**
+     * Agrega un observador al sistema.
+     * @param observerEmergencias Observador a agregar.
+     */
     void agregarObserver(ObserverEmergencias observerEmergencias);
+
+    /**
+     * Remueve un observador del sistema.
+     * @param observerEmergencias Observador a remover.
+     */
     void removerObserver(ObserverEmergencias observerEmergencias);
+
+    /**
+     * Notifica a los observadores cuando se registra una nueva emergencia.
+     * @param emergencia Emergencia registrada.
+     */
     void notificarObservers(Emergencia emergencia);
+
+    /**
+     * Notifica a los observadores cuando una emergencia ha sido resuelta.
+     * @param emergencia Emergencia que ha sido resuelta.
+     */
+    void notificarEmergenciaResuelta(Emergencia emergencia);
+
+    /**
+     * Notifica a los observadores cuando los recursos han sido reasignados.
+     */
+    void notificarRecursosReasignados();
 }
