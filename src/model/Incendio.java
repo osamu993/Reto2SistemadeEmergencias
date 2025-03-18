d63f3a781cf11e9c6b9026ef15b84198e3cd913e
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-03-18 09:47:39 -0500

    feat: Mensaje mejorado en atenderEmergencia para incluir la ubicación del incendio. Mayor claridad en evaluarEstado con un mensaje más detallado. Preparado para refactorizar si Emergencia implementa Responder, reduciendo código repetido.

--- src/model/Incendio.java
+++ src/model/Incendio.java
@@ -3,20 +3,19 @@ package model;
 import controller.Responder;
 import utils.NivelGravedad;
 
-public class Incendio extends Emergencia implements Responder{
+public class Incendio extends Emergencia implements Responder {
 
-    public Incendio (String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta){
+    public Incendio(String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta) {
         super("Incendio", ubicacion, nivelGravedad, tiempoReespuesta);
     }
 
     @Override
     public void atenderEmergencia() {
-        System.out.println("Bomberos en camino!!!");
+        System.out.println("🚒 Bomberos en camino al incendio en " + getUbicacion() + "!!!");
     }
 
     @Override
     public void evaluarEstado() {
-        System.out.println("Revisar personas afectadas");   
+        System.out.println("📋 Evaluando la situación del incendio y verificando personas afectadas.");
     }
-
 }
