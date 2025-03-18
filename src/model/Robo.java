00e458886e76d7f1c21324568f80121a143535d0
Kevin Esteban Sanchez Mendez <kevinesmmss@gmail.com>
2025-03-18 09:48:45 -0500

    .

--- src/model/Robo.java
+++ src/model/Robo.java
@@ -3,20 +3,20 @@ package model;
 import controller.Responder;
 import utils.NivelGravedad;
 
-public class Robo extends Emergencia implements Responder{
+public class Robo extends Emergencia implements Responder {
 
-    public Robo (String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta){
+    public Robo(String ubicacion, NivelGravedad nivelGravedad, int tiempoReespuesta) {
         super("Robo", ubicacion, nivelGravedad, tiempoReespuesta);
     }
 
     @Override
     public void atenderEmergencia() {
-        System.out.println("Policia en camino!!!");
+        System.out.println("🚓 Policía en camino al robo en " + getUbicacion() + "!!!");
     }
 
     @Override
     public void evaluarEstado() {
-        System.out.println("Revisar el estado de los afectados");
+        System.out.println("📋 Evaluando la escena del crimen y verificando el estado de los afectados.");
     }
-
 }
+
